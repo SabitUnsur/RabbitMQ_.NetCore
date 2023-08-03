aa11e4a9f1cc1a92af564c14b46631039ac83a0b
Egemen Unsur <sabitozbilgic2306@gmail.com>
2023-08-03 11:20:48 +0300

    Basic Messsage queue operations

---
+++ RabbitMq.Publisher/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://tegedsqq:ncxR4Qvi_wkErwDukEx0cFuvBEWJCiA_@moose.rmq.cloudamqp.com/tegedsqq");
+
+//Url Bağlantısı
+
+using var connection = factory.CreateConnection(); //RabbitMq bağlantısı açılır.
+
+var channel = connection.CreateModel(); //Kanal oluşturulur, bu kanal üzerinden rabbitmq ile haberleşilir.  
+
+channel.QueueDeclare("hello-queue", true, false, false);
+
+//durable: queue memoryde tutulur rabbitmq restartında tüm kuyruk gider, true derse fiziksel kaydedilir kaybolmaz.
+//exclusive: true yaparsak sadece burada oluşturduğumuz channel üzerinden bağlanabiliriz, biz farklı kanallardan yapacağımız için false dedik
+//autoDelete: Son subscriber da silinirse otomatik kuyruk da silinir.
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    string message = $"Messsage {x}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message); //rabbitmqya mesajlar byte dizin olarak gider.
+
+    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+    //Exchange kullanmadan direkt kuyruğa atacaksak default exchange adı verilir. Kuyruk ismi girilmesi gerekir.
+
+    Console.WriteLine($"Message has been sent : {message}");
+});
+
+//Liste al ve 1 den 50 ye dön ve rabbitmqye gönder.
+
+
+
+Console.ReadLine();
---
+++ RabbitMq.Subscriber/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+factory.Uri = new Uri("amqps://tegedsqq:ncxR4Qvi_wkErwDukEx0cFuvBEWJCiA_@moose.rmq.cloudamqp.com/tegedsqq");
+
+using var connection = factory.CreateConnection(); 
+
+var channel = connection.CreateModel();  
+
+//channel.QueueDeclare("hello-queue", true, false, false);
+//Publisherda bu kuyruğun olduğuna eminsen bu kodu silebilirsin,varsa da parametreler aynı olmalıdır.
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("hello-queue", true , consumer);
+
+//AutoAck: true denince RabbitMQ dan bir mesaj gönderildiğinde doğru da işlense yanlış da işlense kuyruktan silinir.
+// false denirse eğer doğru işlenirse kuyruktan sil, haberdar edeceğim anlamına gelir.
+
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine("Gelen Mesaj : " + message);
+};
+
+//RabbitMq, subscribera mesaj gönderdiğinde bu event fırlar
+
+
+Console.ReadLine();
